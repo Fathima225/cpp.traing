51b7eea3379a75ee1a53d462b946e8713d2303ac
Fathima225 <fathimaniha225@gmail.com>
2025-04-23 12:16:42 +0530

    updated

---
+++ ClassWork/day05/day05.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+using namespace std;
+enum genre {
+	Action,
+	Comedy,
+	Thriller,
+	Mystery,
+	Sifi
+};
+enum language {
+	English=1,
+	Hindi,
+	Tamil,
+	Telugu,
+	Kannada,
+	Marathi,
+	Malayalam
+};
+
+int main()
+{/*
+	int inputLang;
+	char typeShows[20];
+	//char genre[20];
+	int inputGenre;
+
+	cout << "Enter the language option:";
+	cin >> inputLang;
+	switch (inputLang)
+	{
+	case English:
+		cout << "You have choosen English" << endl;
+		cout << "Enter the Genre:";
+		cin >> inputGenre;
+		switch (inputGenre)
+		{
+		default:
+			cout << "No Genre found" << endl;
+			break;
+	case Action:
+		cout << "Choose Genre: Action" << endl;
+		break;
+	case Comedy:
+		cout << "Choose Genre: Comedy" << endl;
+		break;
+	case Thriller:
+		cout << "Choose Genre: Thriller" << endl;
+		break;
+	case Mystery:
+		cout << "Choose Genre: Mystery" << endl;
+		break;
+	
+	
+		
+	}
+		break;
+	case Hindi:
+		cout << "You have choosen Hindi" << endl;
+		cout << "Enter the Genre:";
+		cin >> inputGenre;
+		switch(inputGenre)
+		{
+		case Action:
+			cout << "Choosed Genre:Action" << endl;
+			break;
+		case Comedy:
+			cout << "Choosed Genre:Comedy" << endl;
+			break;
+	
+	*/
+
+	cout << "Hello Fatima" << endl;
+ }
+
+/*enum waterFilter {
+	Cold,
+	Hot,
+	Normal
+};
+
+int main()
+{
+	int inputwaterFilter;
+	cin >> inputwaterFilter;
+	switch (inputwaterFilter)
+	{
+	default:
+		cout << "No inputwaterFilter found" << endl;
+		break;
+	case Cold:
+		cout << "Choose waterFilter: Cold" <<Cold<< endl;
+		break;
+	case  Hot:
+		cout << "Choose waterFilter: Hot" <<Hot<< endl;
+		break;
+	case  Normal:
+		cout << "Choose waterFilter: Normal" <<Normal<< endl;
+		break;
+	}
+}*/
