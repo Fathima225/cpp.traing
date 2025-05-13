49095cd60d173c06819bdf782f1b3a989749f5e9
Fathima225 <fathimaniha225@gmail.com>
2025-05-13 16:14:08 +0530

    updated

--- Assessments/day20/day20/address.cpp
+++ Assessments/day20/day20/address.cpp
@@ -20,37 +20,30 @@ struct address
 	char state[100];
 
 };
-struct book
-{
-	char name[100];
-	char houseNo[100];
-	char street[100];
-	char district[100];
-	char state[100];
 
-};
-
-int add(int);
+int add(struct address a[],int);
 int del(char);
 int search(char);
 int update(char);
-void display();
-int menu(int n);
+void display(struct address a[],int);
+int menu(int);
 
 int main()
 {
-	add('a');
-	return 0;
-	menu('n');
+	
+	struct address a[CAP];
+	int n = 2;
+	add(a, n);
+	display(a,n);
 	return 0;
 
 }
-int add(int n)
+int add(struct address a[],int n)
 {
-
-	cout << "Enter the number of person's address you want to enter" << endl;
+	
+	cout<<"Enter the number of person's address you want to enter" << endl;
 	cin >> n;
-	struct address a[CAP];
+	//struct address a[CAP];
 	for (int i = 0; i < n; i++) {
 		cout << "Enter the name" << endl;
 		cin >> a[i].name;
@@ -62,8 +55,13 @@ int add(int n)
 		cin >> a[i].district;
 		cout << "Enter state" << endl;
 		cin >> a[i].state;
-
 	}
+	return 0;
+}
+
+void display(struct address a[], int n)
+{
+
 	cout << "Address added successfully" << endl;
 	for (int i = 0; i < n; i++) {
 		cout << "Name: " << a[i].name << endl;
@@ -71,24 +69,31 @@ int add(int n)
 		cout << "Street: " << a[i].street << endl;
 		cout << "District: " << a[i].district << endl;
 		cout << "State: " << a[i].state << endl;
+
 	}
-	return 0;
 }
 
+/*
 int del(char name)
 {
 	cout << "Enter the name of the address you want to delete" << endl;
 	cin >> name;
 	struct address a;
-
-	//if(strcmp()
+	
+	/*
+	struct book b;
+	if (strcmp(b.name, a.name) == 0)
+	{
+	true
+	else false
+	
 	cout << "Address deleted successfully" << endl;
 	return 0;
 }
 
 int search(char name)
 {
-	cout << "Enter the name of the address you want to search" << endl;
+		cout << "Enter the name of the address you want to search" << endl;
 	cin >> name;
 	struct address a;
 
@@ -113,8 +118,8 @@ void display()
 	
 }
 
-void menu() {
-	int n;
+int menu(int n)
+{
 	cout << "Enter your choice :" << endl;
 	cin >> n;
 	switch (n) {
@@ -137,4 +142,6 @@ void menu() {
 		cout << "Enter valid one" << endl;
 		break;
 	}
+	return 0;
 }
+*/
